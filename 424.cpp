8a1c5d2691782b5551367e2cbdfedf5797f5638b
EKRAMUL ISLAM SHADIK <48581997+shadikhasan@users.noreply.github.com>
2020-11-25 03:19:40 +0600

    Add files via upload

--- 424.cpp
+++ 424.cpp
@@ -1,31 +1,54 @@
+/******************************************************************************
+
+                       EKRAMUL ISLAM SHADIK
+             Information & Communication Engineering
+                University of Rajshahi,Bangladesh
+*******************************************************************************/
 /*424 - Integer Inquiry*/
+
 #include <bits/stdc++.h>
-#define charToInt(c)(c - '0')
 using namespace std;
+#define ll long long
+#define pb push_back
+#define PI 3.141592653589793
+
 int main()
 {
     string s;
-    long long sum;
+    int sum[101] = { 0 }, temp[101] = { 0 };
     while(1)
     {
         cin >> s;
 
-        for(int i = 0; i < s.length(); i++)
+        if(s == "0")break;
+
+        //operation
+        int carry = 0;
+        int l = s.length();
+        int j = l - 1;
+        for(int i = 0; i < l; i++, j--)
         {
-            int a = charToInt(s[i]);
-
-            //sum digits
-            sum += a;
+            temp[i] = s[j] - 48; // converting string to integer & saved to temp array
         }
-        vector<string> v;
-        v.push_back(sum);
-        if(s == "0")
+        for(int i = 0; i < l; i++)
         {
-            cout << accumulate(v.begin(), v.end(), 0);
-            break;
+            int n = sum[i] + temp[i];
+            sum[i] = n % 10;
+
+            if(n > 9)
+            {
+                sum[i + 1]++; //because carry can be at most one
+            }
         }
-
-
     }
+
+    int i = 101;
+    while(!sum[--i]);
+    for(; i >= 0; i--)
+    {
+        cout << sum[i];
+    }
+    cout << endl;
+
     return 0;
 }
